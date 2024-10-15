9a4cf17847aaece20cf0208225e6f5fd2274a347
h4ckx0r <natanagoor@gmail.com>
2024-10-15 13:29:10 +0200

    Suma

--- src/main/java/net/h4ckx0r/Calculadora.java
+++ src/main/java/net/h4ckx0r/Calculadora.java
@@ -3,5 +3,6 @@ package net.h4ckx0r;
 public class Calculadora {
     public static void main(String[] args) {
         System.out.println("== Calculadora ==");
+        System.out.println("1. Suma");
     }
 }
